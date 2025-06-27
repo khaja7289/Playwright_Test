690b2812f930a79cabba48b146cf7b49bc460a87
Junjurampalli Khaja <junjurampalli.10738962@ltimindtree.com>
2025-06-27 11:39:59 +0530

    Updated workflow file and artifacts path

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['json', {outputFile:'Test-Result/Report_json.json'}],
             ['junit', {outputFile:'Test-Result/Report_junit.xml'}],
-            ["allure-playwright"]
+            ["allure-playwright"],
+            ["html"]
             ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
